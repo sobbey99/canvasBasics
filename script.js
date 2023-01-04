0c0bc2a9ed84f9be494005102cc705b7f09b587c
sobbey99 <sobbey99@gmail.com>
2023-01-04 13:21:41 +0200

    Other Way to draw

--- script.js
+++ script.js
@@ -16,9 +16,21 @@ const mouse = {
     y: undefined,
 }
 
+canvas.addEventListener('click', function(e){
+    mouse.x = e.x;
+    mouse.y = e.y;
+    for(let i =0; i < 20; i++) {
+        particlesArray.push(new Particle('white'));
+    }
+});
+
+
 canvas.addEventListener('mousemove', function(e){
     mouse.x = e.x;
     mouse.y = e.y;
+    for(let i =0; i < 5; i++) {
+        particlesArray.push(new Particle('white'));
+    }
 });
 
 
@@ -26,49 +38,52 @@ canvas.addEventListener('mousemove', function(e){
 
 
 class Particle {
-    constructor() {
-    //  this.x = mouse.x;
-    //  this.y = mouse.y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-     this.size = Math.random() * 5 + 1;
+    constructor(color) {
+     this.x = mouse.x;
+     this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
+     this.size = Math.random() * 15 + 1;
      this.speedX = Math.random() * 3 - 1.5;
      this.speedY = Math.random() * 3 - 1.5;
+     this.color = color;
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
-    }
+        if ( this.size > 0.2 ) {
+            this.size -= 0.1;
+        }
+     }
 
     draw(){
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = this.color;
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.arc(this.x,this.y, 30 , 0, Math.PI * 2);
+        ctx.arc(this.x,this.y, this.size , 0, Math.PI * 2);
         ctx.fill();
     }
 }
 
-function init(){
- for(let i = 0; i < 100; i++){
-    particlesArray.push(new Particle());
- }
-}
 
-init();
 
 function handleParticles(){
     for(let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
-
+        if(particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            i--;
+        }
     }
 }
 
 
 function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    // ctx.clearRect(0,0, canvas.width, canvas.height);
     //ctx.clearRect(start X, start Y, end X, end Y)
+    ctx.fillStyle = 'rgba(0,0,0,0.1)';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
     handleParticles()
     requestAnimationFrame(animate);
     // requestAnimationFrame request a function one time in frame, but in case above in call itself over and over and creates infinity loop
